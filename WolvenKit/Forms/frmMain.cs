29e09bd7fc067f40fd7562851ef5abd1992ea93b
Ákos Köte <kote2ster@gmail.com>
2018-04-20 19:19:34 +0200

    Renderer: Refactor variables

--- WolvenKit/Forms/frmMain.cs
+++ WolvenKit/Forms/frmMain.cs
@@ -912,8 +912,12 @@ namespace WolvenKit
 
         public CR2WFile LoadDocumentAndGetFile(string filename)
         {
+            foreach (var t in OpenDocuments.Where(t => t.FileName == filename))
+                return t.File;
+            var activedoc = OpenDocuments.FirstOrDefault(d => d.IsActivated);
             var doc = LoadDocument(filename);
-            return doc.File;
+            activedoc.Activate();
+            return doc != null ? doc.File : null;
         }
         #endregion //Methods
 
